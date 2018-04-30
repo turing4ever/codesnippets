db25c3b1996bf574d643850c0974a99b9bd3be78
Yi Wang <turing4ever@gmail.com>
2018-04-30 10:00:02 -0700

    keyword arugment

--- py3_lab/py3code.py
+++ py3_lab/py3code.py
@@ -1,7 +1,13 @@
-
 # place super_test.py code here
 
 
 # place keyword_test.py code here
+def force(*, mass, acceleration):
+    return mass*acceleration
 
 
+def quad(*, a, b, c):
+    sqrt = (b**2 - 4*a*c)**0.5
+    x1 = (-b + sqrt)/(2*a)
+    x2 = (-b - sqrt)/(2*a)
+    return x1, x2
