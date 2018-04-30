704ffcbff32d2cc4b551e48f850623ac6bb270db
Yi Wang <turing4ever@gmail.com>
2018-04-29 22:31:08 -0700

    __matmul__

--- py3_lab/mul_test.py
+++ py3_lab/mul_test.py
@@ -6,10 +6,18 @@ def test_mul():
     # operator to return the dot product.
     # (multiply each corresponding value, then sum results)
     # ***********************************
+    class Vector:
+        def __init__(self, lst):
+            self.lst = lst
+        def __mul__(self, other):
+            return sum(x*y for x,y in zip(self.lst, other.lst)) 
+        def __matmul__(self, other):
+            return sum(x*y for x,y in zip(self.lst, other.lst)) 
 
     v1 = Vector([1, 2, 3])
     v2 = Vector([4, 5, 6])
     assert v1@v2 == 32
+    assert v1*v2 == 32
 
         
 if __name__ == '__main__':
