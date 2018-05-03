148eb4656687cb04a55c189b08159aecfe625a7a
Yi Wang <turing4ever@gmail.com>
2018-05-02 22:01:09 -0700

    unpack

--- py3_lab/unpack_test.py
+++ py3_lab/unpack_test.py
@@ -7,7 +7,7 @@ def test_unpack():
     # ***********************************
 
     person = ('Candide', 30, 'France')
-
+    name, age, country = person
     assert (name, age, country) == ('Candide', 30, 'France')
 
     # Extended Unpacking
@@ -15,13 +15,13 @@ def test_unpack():
     # Use unpacking to get the first letter
     # of name. Store the result in ``first``
     # ***********************************
-
+    (first, *d), *ignore = person 
     assert first == 'C'
 
     # Unpack the characters from ``name`` into a
     # list called ``letters``
     # ***********************************
-
+    (*letters,), *ignore = person
     assert letters == ['C', 'a', 'n', 'd', 'i', 'd', 'e'] 
 
 if __name__ == '__main__':
