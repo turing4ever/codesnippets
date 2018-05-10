774c0e39b1fa9e385a37cc8e48e61299b4ff707b
Michael Wang <turing4ever@gmail.com>
2018-05-09 19:03:21 -0700

    check current environment

---
+++ sql/current_info.sql
@@ -0,0 +1,17 @@
+-- Those SQL queries work on both Redshift and Postgres
+-- get current user
+select user;
+select current_user;
+
+-- get current database
+select current_database();
+
+-- get version
+select version();
+
+-- get datetime
+select now();
+
+-- get current date
+select current_date;
+
