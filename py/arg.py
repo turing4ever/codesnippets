d96e5ab16b043aac810c65d627f526503e1d7877
Yi Wang <turing4ever@gmail.com>
2018-06-01 15:53:27 -0700

    add subcommand

--- py/arg.py
+++ py/arg.py
@@ -1,12 +1,22 @@
 import argparse
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('integers', metavar='N', type=int, nargs='+',
-        help='an integer for the accumulator')
-    parser.add_argument('--sum', dest='accumulate', action='store_const',
-        const=sum, default=max,
-        help='sum the integers (default: find the max)')
+def main():
+    # create the top-level parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--foo', action='store_true', help='foo help')
+    subparsers = parser.add_subparsers(help='sub-command help')
+    
+    # create the parser for the "a" command
+    parser_a = subparsers.add_parser('a', help='a help')
+    parser_a.add_argument('--bar', type=int, help='bar help')
+    
+    # create the parser for the "b" command
+    parser_b = subparsers.add_parser('b', help='b help')
+    parser_b.add_argument('--baz', choices='XYZ', help='baz help')
 
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
+    print(args)
+
+
+if __name__ == '__main__':
+    main()
