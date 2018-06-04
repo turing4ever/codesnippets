155885bf3c83ac0c259cb52265d125438e138ca8
Yi Wang <turing4ever@gmail.com>
2018-06-03 18:12:23 -0700

    simple sub commands with argparse

--- py/arg.py
+++ py/arg.py
@@ -1,22 +1,24 @@
 import argparse
 
-def main():
-    # create the top-level parser
+def tt():
+    """try out the code in tutorial"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('--foo', action='store_true', help='foo help')
-    subparsers = parser.add_subparsers(help='sub-command help')
-    
-    # create the parser for the "a" command
-    parser_a = subparsers.add_parser('a', help='a help')
-    parser_a.add_argument('--bar', type=int, help='bar help')
-    
-    # create the parser for the "b" command
-    parser_b = subparsers.add_parser('b', help='b help')
-    parser_b.add_argument('--baz', choices='XYZ', help='baz help')
-
+    subparsers = parser.add_subparsers(dest='cmd')
+    parser_echo = subparsers.add_parser('echo', help='echo help')
+    parser_echo.add_argument("echo")
+    parser_square = subparsers.add_parser('square', help="display a square of a given number")
+    parser_square.add_argument("square", help="Calculate the square", type=float)
+    parser_square.add_argument("-v", "--verbose", action="store_true",
+                    help="increase output verbosity")
     args = parser.parse_args()
-    print(args)
-
+    if args.cmd and args.cmd=='echo':
+        print(args.echo)
+    if args.cmd and args.cmd=='square':
+        answer = args.square**2
+        if args.verbose:
+            print("the square of {} equals {}".format(args.square, answer))
+        else:
+            print(answer)
 
 if __name__ == '__main__':
-    main()
+    tt()
