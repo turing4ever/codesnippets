735ee7deffe925171dfbb33571f7a3ed7f0c0de2
Yi Wang <turing4ever@gmail.com>
2018-08-07 10:29:47 -0700

    update 1.two-sum.python3.py

--- leetcode/1.two-sum.python3.py
+++ leetcode/1.two-sum.python3.py
@@ -5,4 +5,7 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        return [] 
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                if target == (nums[i] + nums[j]):
+                    return [i, j] 
