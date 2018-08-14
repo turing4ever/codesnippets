cd2a2e32efca85724d28d9a337abc5975c9c1c67
Yi Wang <turing4ever@gmail.com>
2018-08-13 22:30:12 -0700

    update 26.remove-duplicates-from-sorted-array.python3.py

--- leetcode/26.remove-duplicates-from-sorted-array.python3.py
+++ leetcode/26.remove-duplicates-from-sorted-array.python3.py
@@ -9,7 +9,7 @@ class Solution:
         tail = 0
         for i in range(1, len(nums)):
             if nums[i] != nums[tail]:
-                tail += 1
+                tail = tail + 1
                 nums[tail] = nums[i]
         return tail+1
 
