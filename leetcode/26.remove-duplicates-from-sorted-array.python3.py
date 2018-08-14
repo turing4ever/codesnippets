1658fcc96870035264613613ff74f2b3e7e0cf5c
Yi Wang <turing4ever@gmail.com>
2018-08-13 22:12:35 -0700

    update 26.remove-duplicates-from-sorted-array.python3.py

--- leetcode/26.remove-duplicates-from-sorted-array.python3.py
+++ leetcode/26.remove-duplicates-from-sorted-array.python3.py
@@ -4,11 +4,15 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        distinct = [] 
-        for n in nums:
-            if n not in distinct:
-                distinct.append(n)
-        for i in range(len(distinct)):
-            nums[i] = distinct[i]
-        return len(distinct)
+        if not nums:
+            return 0
+        tail = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                if nums[tail] == nums[i-1]:
+                    nums[tail+1] = nums[i]
+                tail += 1
+            else:
+                pass # tail doesn't move
+        return tail+1
 
