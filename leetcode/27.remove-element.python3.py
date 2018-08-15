578e05c5dbe3bfff22222d82cd26419e1229ebf7
Yi Wang <turing4ever@gmail.com>
2018-08-14 22:24:41 -0700

    update 27.remove-element.python3.py

--- leetcode/27.remove-element.python3.py
+++ leetcode/27.remove-element.python3.py
@@ -5,19 +5,12 @@ class Solution:
         :type val: int
         :rtype: int
         """
-        if len(nums) == 0:
-            return 0
-        head = 0 
-        tail = len(nums) - 1
+        head, tail = 0, len(nums) - 1
         while head <= tail:
-            while tail >= 0 and nums[tail] == val:
-                tail -= 1
-            while head <= len(nums)-1 and nums[head] != val:
-                head += 1       
-            if head > tail:
-                break
-            if nums[head] == val and nums[tail] != val:
+            if nums[head] == val:
                 nums[head], nums[tail] = nums[tail], nums[head]
-
+                tail -= 1
+            else:
+                head += 1
         return head
                 
