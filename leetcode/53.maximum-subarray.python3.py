8377bf31f9dbff4547cf561199fc44710f8d4fc0
Yi Wang <turing4ever@gmail.com>
2018-08-16 22:00:57 -0700

    update 53.maximum-subarray.python3.py

--- leetcode/53.maximum-subarray.python3.py
+++ leetcode/53.maximum-subarray.python3.py
@@ -8,12 +8,6 @@ class Solution:
         #    return 
         max_all = max_tmp = nums[0]
         for n in nums[1:]:
-            if max_tmp >= 0:
-                max_tmp = max_tmp + n
-            else:
-                if n < 0:
-                    pass
-                elif n >=0:
-                    max_tmp = n
-            max_all = max(max_all, n, max_tmp)
+            max_tmp = max(n, n+max_tmp)
+            max_all = max(max_all, max_tmp)
         return max_all
