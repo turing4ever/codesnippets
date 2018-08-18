1d56defca31ec1cf7abf3c9559fcfebd7331b665
Yi Wang <turing4ever@gmail.com>
2018-08-18 10:25:21 -0700

    update 118.pascals-triangle.python3.py

--- leetcode/118.pascals-triangle.python3.py
+++ leetcode/118.pascals-triangle.python3.py
@@ -6,11 +6,11 @@ class Solution:
         """
         result = []
         for level in range(numRows):
-            if level > 0:
-                n = [result[level-1][i-1]+ result[level-1][i] if i>0 and i<level else 1 for i in range(level+1)]
-                result.append(n)
+            if level == 0:
+                result += [[1]]
             else:
-                result.append([1])
+                n = [result[level-1][i-1] + result[level-1][i] if i>0 and i<level else 1 for i in range(level+1)]
+                result += [n]
         return result
                  
         
