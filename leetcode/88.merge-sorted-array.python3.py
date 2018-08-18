70745a48badeb31ac5442589e695355ad759bd17
Yi Wang <turing4ever@gmail.com>
2018-08-17 23:04:27 -0700

    update 88.merge-sorted-array.python3.py

---
+++ leetcode/88.merge-sorted-array.python3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        last1 = m - 1
+        last2 = n - 1
+        last = m + n - 1
+        while last2 >= 0:
+            if last1 == -1:
+                nums1[last] = nums2[last2]
+                last2 -= 1
+            else:
+                if nums1[last1] >= nums2[last2]:
+                    nums1[last] = nums1[last1]
+                    last1 -= 1
+                else:
+                    nums1[last] = nums2[last2]
+                    last2 -= 1
+            last -= 1
+
+
