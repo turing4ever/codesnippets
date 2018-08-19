f576ccffb913f27bff3eedb54711fb43d1464c01
Yi Wang <turing4ever@gmail.com>
2018-08-18 22:09:02 -0700

    update 119.pascals-triangle-ii.python3.py

--- leetcode/119.pascals-triangle-ii.python3.py
+++ leetcode/119.pascals-triangle-ii.python3.py
@@ -4,18 +4,7 @@ class Solution:
         :type rowIndex: int
         :rtype: List[int]
         """
-        tmp = [[0]*(rowIndex+1)] + [[0]*(rowIndex+1)]
-        for row in range(rowIndex+1):
-            if row == 0:
-                tmp[0][0] = 1
-            else:
-                i = row % 2
-                last = (row-1) % 2
-                for col in range(row+1):
-                    if col > 0 and col < row:
-                        tmp[i][col] = tmp[last][col-1] + tmp[last][col]
-                    else:
-                        tmp[i][col] = 1
-        return tmp[rowIndex%2][:rowIndex+1]
-
-        
+        row = [1]
+        for _ in range(rowIndex):
+            row = [x+y for x,y in zip([0]+row, row+[0])]
+        return row
