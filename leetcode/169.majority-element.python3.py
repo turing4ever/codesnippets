a096e8b95861bb74da8db20d56fb44734cc2a716
Yi Wang <turing4ever@gmail.com>
2018-08-19 09:59:57 -0700

    update 169.majority-element.python3.py

--- leetcode/169.majority-element.python3.py
+++ leetcode/169.majority-element.python3.py
@@ -4,11 +4,4 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        d = {}
-        for n in nums:
-            if n not in d:
-                d[n] = 1
-            else:
-                d[n] += 1
-            if d[n] >= len(nums)/2:
-                return n
+        return sorted(nums)[len(nums)//2]
