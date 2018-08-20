3b1b50f9edb62996eb7a3b202c29fa6ae13179f6
Yi Wang <turing4ever@gmail.com>
2018-08-19 22:07:34 -0700

    update 217.contains-duplicate.python3.py

--- leetcode/217.contains-duplicate.python3.py
+++ leetcode/217.contains-duplicate.python3.py
@@ -4,7 +4,8 @@ class Solution:
         :type nums: List[int]
         :rtype: bool
         """
-        if len(set(nums)) < len(nums):
-            return True
-        else:
-            return False
+        sort = sorted(nums)
+        for i in range(1, len(sort)):
+            if sort[i-1] == sort[i]:
+                return True
+        return False
