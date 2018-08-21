4b20fd61aea2f315a3ae056412b2ab08de850f3e
Yi Wang <turing4ever@gmail.com>
2018-08-21 09:45:10 -0700

    update 414.third-maximum-number.python3.py

--- leetcode/414.third-maximum-number.python3.py
+++ leetcode/414.third-maximum-number.python3.py
@@ -4,10 +4,27 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        max1 = max(nums)
-        try:
-            max2 = max(set(nums) - {max1})
-            max3 = max(set(nums) - {max1, max2})
-            return max3
-        except:
-            return max1
+        m = [] 
+        for i in nums:
+            if len(m) == 0:
+                m.append(i)
+            elif len(m) < 3:
+                if i < m[0]:
+                    m.insert(0, i)
+                elif i > m[-1]:
+                    m.append(i)
+                elif i > m[0] and i < m[-1]:
+                    m.insert(1, i)
+            elif len(m) == 3:
+                if i > m[0] and i < m[1]:
+                    m[0] = i
+                elif i > m[1] and i < m[2]:
+                    m.insert(2, i)
+                    m.pop(0)
+                elif i > m[2]:
+                    m.insert(3, i)
+                    m.pop(0)
+        if len(m) == 3:
+            return m[0]
+        else:
+            return m[-1]
