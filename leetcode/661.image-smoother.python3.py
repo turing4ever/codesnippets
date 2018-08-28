cdb72831162f5f189a11f1c61bd637c6009a5cf0
Yi Wang <turing4ever@gmail.com>
2018-08-27 20:01:04 -0600

    update 661.image-smoother.python3.py

--- leetcode/661.image-smoother.python3.py
+++ leetcode/661.image-smoother.python3.py
@@ -4,18 +4,18 @@ class Solution:
         :type M: List[List[int]]
         :rtype: List[List[int]]
         """
-        n = len(M)
-        def avg(M, i, j):
-            running_sum = 0
-            cnt = 0
-            rows = [i-1, i, i+1]
-            cols = [j-1, j, j+1]
-            for r,c in [(r, c) for r in rows for c in cols]:
-                if r >= 0 and r <= n-1 and c >=0 and c<=len(M[r])-1:
-                    running_sum += M[r][c]
-                    cnt += 1
-            return int(running_sum/cnt)
         ret = []
-        for i in range(n):
-            ret.append([avg(M, i, j) for j in range(len(M[i]))])
+        for i in range(len(M)):
+            new_row = []
+            for j in range(len(M[i])):
+                running_sum = 0
+                cnt = 0
+                rows = [i-1, i, i+1]
+                cols = [j-1, j, j+1]
+                for r,c in [(r, c) for r in rows for c in cols]:
+                    if r >= 0 and r <= len(M)-1 and c >=0 and c<=len(M[r])-1:
+                        running_sum += M[r][c]
+                        cnt += 1
+                new_row.append(int(running_sum/cnt))
+            ret.append(new_row)
         return ret
