03921ab126cccb114d3e0a286b5333e8f7f75985
Yi Wang <turing4ever@gmail.com>
2018-08-28 19:54:59 -0600

    update 674.longest-continuous-increasing-subsequence.python3.py

--- leetcode/674.longest-continuous-increasing-subsequence.python3.py
+++ leetcode/674.longest-continuous-increasing-subsequence.python3.py
@@ -4,14 +4,16 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) == 0:
-            return 0
-        maxi = 1
-        cnt = 1 
-        for i in range(1, len(nums)):
-            if nums[i-1] < nums[i]:
-                cnt += 1
+        maxi = 0 
+        cnt = 0 
+        for i in range(len(nums)):
+            if i-1 >= 0:
+                if nums[i-1] < nums[i]:
+                    cnt += 1
+                else:
+                    cnt = 1 
             else:
                 cnt = 1
+                mamxi = 1
             maxi = max(maxi, cnt)
         return maxi
