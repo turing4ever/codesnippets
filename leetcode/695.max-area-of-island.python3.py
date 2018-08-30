1c5ad287d2bfbbb36c60bc53476b2ac438be6eb9
Yi Wang <turing4ever@gmail.com>
2018-08-29 22:43:33 -0700

    update 695.max-area-of-island.python3.py

---
+++ leetcode/695.max-area-of-island.python3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxAreaOfIsland(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        def ones(grid, i, j):
+            if grid[i][j] == 1:
+                grid[i][j] = 0 
+                yield i,j
+                if j-1 >= 0:
+                    yield from ones(grid, i, j-1)
+                if j+1 < len(grid[i]):
+                    yield from ones(grid, i, j+1)
+                if i-1 >= 0 :
+                    yield from ones(grid, i-1, j)
+                if i+1 < len(grid): 
+                    yield from ones(grid, i+1, j)
+
+        maxi = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                cnt = 0
+                for p in ones(grid, i, j):
+                    cnt += 1
+                maxi = max(maxi, cnt)
+        return maxi
