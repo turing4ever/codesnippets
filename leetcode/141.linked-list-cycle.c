101c417e1a0b506a85cd677f460b6a58132355f0
Yi Wang <turing4ever@gmail.com>
2018-09-02 23:03:18 -0700

    update 141.linked-list-cycle.c

--- leetcode/141.linked-list-cycle.c
+++ leetcode/141.linked-list-cycle.c
@@ -6,15 +6,12 @@
  * };
  */
 bool hasCycle(struct ListNode *head) {
-    struct ListNode *p1 = head;
-    struct ListNode *p2 = head;
-    while(p1 && p2)
+    struct ListNode *p1 = head, *p2 = head;
+    while(p1 && p2 && p2->next)
     {
         p1 = p1->next;
-        p2 = p2->next;
-        if(p2)
-            p2 = p2->next;
-        if(p2 && p1==p2)
+        p2 = p2->next->next;
+        if(p1 && p1==p2)
             return true;
     }
     return false; 
