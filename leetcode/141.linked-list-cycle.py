d96230e789c7c6f1d6baf74b5ebbfc78a8a8c479
Yi Wang <turing4ever@gmail.com>
2018-09-02 22:51:49 -0700

    update 141.linked-list-cycle.py

---
+++ leetcode/141.linked-list-cycle.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head:
+            p1 = p2 = head
+            while p1 and p2:
+                p1 = p1.next
+                p2 = p2.next
+                if p2:
+                    p2 = p2.next
+                if p2 and p2.next == p1:
+                    return True
+        return False
