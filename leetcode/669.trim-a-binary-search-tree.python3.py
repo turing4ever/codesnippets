20981d8c59daa8b643150eaa9daa87a10bbf98fa
Yi Wang <turing4ever@gmail.com>
2018-09-17 10:24:55 -0700

    update 669.trim-a-binary-search-tree.python3.py

--- leetcode/669.trim-a-binary-search-tree.python3.py
+++ leetcode/669.trim-a-binary-search-tree.python3.py
@@ -13,13 +13,12 @@ class Solution:
         :type R: int
         :rtype: TreeNode
         """
-        if not root:
-            return None
-        if L > root.val:
-            return self.trimBST(root.right, L, R)
-        elif R < root.val:
-            return self.trimBST(root.left, L, R)
-        root.left = self.trimBST(root.left, L, R)
-        root.right = self.trimBST(root.right, L, R)
-        return root
-        
+        if root:
+            if root.val < L:
+                return self.trimBST(root.right, L, R)
+            elif root.val > R:
+                return self.trimBST(root.left, L, R)
+            else:
+                root.left = self.trimBST(root.left, L, R)
+                root.right = self.trimBST(root.right, L, R)
+        return root 
