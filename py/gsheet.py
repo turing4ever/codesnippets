62bc53eaf6d7816061c8af3dcc89487d4bb637f2
Yi Wang <turing4ever@gmail.com>
2018-09-28 16:55:34 -0700

    sample code for python threading and gspread

---
+++ py/gsheet.py
@@ -0,0 +1,17 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+gc = gspread.authorize(credentials)
+
+# Open a worksheet from spreadsheet with one shot
+wks = gc.open("test_google_sheet").sheet1
+
+wks.update_acell('B2', "it's working.")
+
+# Fetch a cell range
+cell_list = wks.range('A1:B7')
---
+++ py/thread.py
@@ -0,0 +1,18 @@
+import threading
+import time
+
+def worker(*args):
+    time.sleep(1)
+    print(f"{threading.current_thread()}, {args}")
+
+tasks = list(range(10))
+threads = []
+for i in range(4):
+    t = threading.Thread(target=worker, args=(tasks[4*i: 4*(i+1)],))
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+print(f"{threading.main_thread()}, all done")
