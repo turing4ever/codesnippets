ef6166bd770a43e5172e4963d9d74c4687bbe694
Yi <turing4ever@gmail.com>
2018-11-16 10:15:07 -0800

    Added basicConfig to enable logger output to stdout.

--- py/tt.py
+++ py/tt.py
@@ -1,13 +1,73 @@
-import random
-from tenacity import retry
-from tenacity import stop
+import logging
+import sys
+from tenacity import retry, stop, wait, wait_fixed, \
+    wait_random, stop_after_attempt, stop_after_delay,\
+    wait_exponential, before_log
 
-@retry(stop=stop.stop_after_attempt(7))
-def do_something_unreliable():
-    r = random.randint(1, 100)
-    if  r > 1:
-        raise IOError("Broken sauce, everything is hosed!!!111one")
-    else:
-        return "Awesome sauce!"
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+LOGGER = logging.getLogger(__name__)
 
-print(do_something_unreliable())
+# def random_failure():
+#     r = random.randint(0, 100)
+#     if r > 50:
+#         print(r)
+#         raise IOError("Broken, failed")
+#     else:
+#         return "All done!"
+#
+# @retry
+# def never_give_up_never_surrender():
+#     print("Retry forever ignoring Exceptions, don't wait between retries")
+#     raise Exception
+#
+#
+# @retry(stop=stop_after_attempt(7))
+# def stop_after_7_attempts():
+#     print("Stopping after 7 attempts")
+#     raise Exception
+#
+#
+# @retry(stop=stop_after_delay(10))
+# def stop_after_10_s():
+#     print("Stopping after 10 seconds")
+#     raise Exception
+#
+#
+# @retry(stop=(stop_after_delay(10) | stop_after_attempt(5)))
+# def stop_after_10_s_or_5_retries():
+#     print("Stopping after 10 seconds or 5 retries")
+#     raise Exception
+#
+#
+# @retry(wait=wait_fixed(2))
+# def wait_2_s():
+#     print("Wait 2 second between retries")
+#     raise Exception
+#
+#
+# @retry(reraise=True, stop=stop_after_attempt(4), wait=wait_exponential(multiplier=1, min=4, max=10))
+# def wait_exponential_1():
+#     print("Wait 2^x * 1 sec between each retry starting with 4 sec, then up to 10 sec, then 10 sec afterwards")
+#     raise Exception
+#
+#
+# @retry(reraise=True, stop=stop_after_attempt(4), wait=wait_random(min=1, max=2))
+# def raise_my_exception():
+#     raise Exception("Fail")
+#
+
+
+@retry(reraise=True,
+       stop=stop_after_attempt(4),
+       wait=wait_random(min=1, max=2),
+       before=before_log(LOGGER, logging.DEBUG))
+def wait_random_1_to_2_s():
+    raise Exception
+
+
+if __name__ == '__main__':
+    try:
+        wait_random_1_to_2_s()
+    except Exception:
+        pass
+    print(wait_random_1_to_2_s.retry.statistics)
