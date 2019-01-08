62c8c310221d68accb36338da86ab793e4736fee
Yi Wang <turing4ever@gmail.com>
2019-01-08 09:54:39 -0800

    update 501.find-mode-in-binary-search-tree.python3.py

--- leetcode/501.find-mode-in-binary-search-tree.python3.py
+++ leetcode/501.find-mode-in-binary-search-tree.python3.py
@@ -4,7 +4,6 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-from collections import Counter
 
 class Solution:
     def findMode(self, root):
@@ -12,16 +11,33 @@ class Solution:
         :type root: TreeNode
         :rtype: List[int]
         """
-        def traverse(node, freq):
+        if not root:
+            return []
+        def inorder(node):
             if node:
-                freq[node.val] += 1
-                traverse(node.left, freq)
-                traverse(node.right, freq)
-        freq = Counter() 
-        traverse(root, freq)
-        top = freq.most_common()
-        ret = []
-        if top:
-            ret = [x[0] for x in top if x[1] == top[0][1]]
-        return ret
+                if node.left:
+                    yield from inorder(node.left)
+                yield node.val
+                if node.right:
+                    yield from inorder(node.right)
         
+        out = []
+        max_num = 0
+        cur_num = 0
+        cur_v = -1
+        for v in inorder(root):
+            if cur_v != v:
+                if cur_num == max_num:
+                    out.append(cur_v)
+                elif cur_num > max_num:
+                    out = [cur_v]
+                    max_num = cur_num
+                cur_v = v
+                cur_num = 0
+            cur_num += 1
+        else:
+            if cur_num == max_num:
+                out.append(cur_v)
+            elif cur_num > max_num:
+                out = [cur_v]
+        return out
